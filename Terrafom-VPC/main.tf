f47ab1e45d4dfd0a93b9dfaca47dc759f15f12ca
Shetty <269570@NTTDATA.COM>
2023-10-11 17:49:20 +0530

    adding workflows

--- Terrafom-VPC/main.tf
+++ Terrafom-VPC/main.tf
@@ -17,9 +17,9 @@ module "ec2" {
 }
 module "alb" {
     source = "./modules/alb"
-     sg_id = module.sg.sg_id
-       subnets = module.vpc.subnet_ids
-     vpc_id = module.vpc.vpc_id
+    sg_id = module.sg.sg_id
+    subnets = module.vpc.subnet_ids
+    vpc_id = module.vpc.vpc_id
     instances = module.ec2.instances
      
    
